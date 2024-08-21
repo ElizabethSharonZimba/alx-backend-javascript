a98017a4422f5271bf024ea52c637bbec700dd50
ElizabethSharonZimba <sharonzimba95@gmail.com>
2024-08-21 00:01:41 +0000

    Re-commit files after fixing repository issues

---
+++ 0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Logs a message to the console.
+ * @param {string} message - The message to log.
+ * @returns {void}
+ */
+// Arrow function
+const displayMessage = (message) => console.log(message);
+
+module.exports = displayMessage;
