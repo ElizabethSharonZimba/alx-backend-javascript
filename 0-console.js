15a71aa4bcb78f7bb4fc4eb7d25f956096f112dc
ElizabethSharonZimba <sharonzimba95@gmail.com>
2024-09-20 10:51:26 +0000

    restored my lost files

--- 0-console.js
+++
@@ -1,9 +0,0 @@
-/**
- * Logs a message to the console.
- * @param {string} message - The message to log.
- * @returns {void}
- */
-// Arrow function
-const displayMessage = (message) => console.log(message);
-
-module.exports = displayMessage;
